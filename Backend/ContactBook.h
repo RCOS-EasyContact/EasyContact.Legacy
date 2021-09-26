88d8ccee35e233f90dd58b6ef3434eaef422bc05
Zhenyuan Gong <79224316+reC4P7CH4@users.noreply.github.com>
2021-09-26 01:41:54 -0400

    Error Checking

--- Backend/ContactBook.h
+++ Backend/ContactBook.h
@@ -6,13 +6,24 @@
  */
 #ifndef BACKEND_CONTACTBOOK_H_
 #define BACKEND_CONTACTBOOK_H_
+#include <exception>
+#include <iostream>
+#include <string>
+#include <vector>
 #include "GroupContact.h"
 class ContactBook {
   std::vector<GroupContact> _BOOKS;
   inline ssize_t __H_ExistsGroup(const std::string &GroupName);
 
-public:
+ public:
   ContactBook() = default;
+  const GroupContact &getGroup(const std::string &GroupName) {
+    try {
+      return _BOOKS[__H_ExistsGroup(GroupName)];
+    } catch (std::exception &ERR) {
+      std::cerr << "ContactBook: " << ERR.what() << std::endl;
+    }
+  }
   bool addNewGroup(const std::string &GroupName) {
     const ssize_t GroupIndex = __H_ExistsGroup(GroupName);
     if (GroupIndex == -1) {
@@ -30,15 +41,19 @@ public:
   }
   friend std::ostream &operator<<(std::ostream &FILE,
                                   const ContactBook &Input) {
-    for (const GroupContact &N : Input._BOOKS) {
-      FILE << N << " | ";
+    try {
+      for (const GroupContact &N : Input._BOOKS) {
+        FILE << N << " | ";
+      }
+    } catch (std::exception &ERR) {
+      std::cerr << "ContactBook: " << ERR.what() << std::endl;
     }
     return FILE;
   }
-  inline bool operator+=(const std::string &GroupName){
+  inline bool operator+=(const std::string &GroupName) {
     return this->addNewGroup(GroupName);
   }
-  GroupContact& operator[](const uint64_t &GroupIndex){
+  GroupContact &operator[](const uint64_t &GroupIndex) {
     return _BOOKS[GroupIndex];
   }
 };
@@ -47,12 +62,16 @@ public:
  * ContactBook::__H_ExistsGroup()
  */
 ssize_t ContactBook::__H_ExistsGroup(const std::string &GroupName) {
-  for (size_t i = 0; i < _BOOKS.size(); i++) {
-    if (_BOOKS[i].getGroupName() == GroupName) {
-      return (ssize_t)i;
+  try {
+    for (size_t i = 0; i < _BOOKS.size(); i++) {
+      if (_BOOKS[i].getGroupName() == GroupName) {
+        return (ssize_t)i;
+      }
     }
+  } catch (std::exception &ERR) {
+    std::cerr << "ContactBook: " << ERR.what() << std::endl;
   }
   return (ssize_t)-1;
 }
 
-#endif // BACKEND_CONTACTBOOK_H_
+#endif  // BACKEND_CONTACTBOOK_H_
