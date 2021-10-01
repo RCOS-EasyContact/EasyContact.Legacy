34de0c5f25eaef2d3cea613711c694436f5e4d76
Henry Hongbin Liu <78756514+hayTambourineMan@users.noreply.github.com>
2021-10-01 16:54:46 -0400

    Resourses.h

---
+++ Backend/ContactSystem/Resources.h
@@ -0,0 +1,34 @@
+#ifndef RCOS_EASYCONTACT_DATABASE
+#define RCOS_EASYCONTACT_DATABASE
+
+// DEFINE
+#define BYPASSUNUSED(X) (void)(X)
+using namespace std;
+// C LIBIARY
+#include <fcntl.h>
+#include <math.h>
+#include <signal.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+#include <unistd.h>
+// C++ LIBIARY
+#include <algorithm>
+#include <deque>
+#include <fstream>
+#include <iostream>
+#include <iterator>
+#include <list>
+#include <memory>
+#include <queue>
+#include <string>
+#include <utility>
+#include <vector>
+// LOCAL LIBIARY
+#include "Contact.h"
+#include "ContactBook.h"
+#include "GroupContact.h"
+#include "Import.h"
+
+#endif
