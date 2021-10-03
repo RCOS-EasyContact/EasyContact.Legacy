80865aa53314924f88700e3966aefa652ee8543a
jacky <jackyxu866@gmail.com>
2021-10-03 02:41:42 -0400

    Fix more Lint error

--- Backend/APIRouter.h
+++ Backend/APIRouter.h
@@ -7,12 +7,11 @@
 #ifndef BACKEND_APIROUTER_H_
 #define BACKEND_APIROUTER_H_
 
-//#include "ConatctSystem/Resources.h"
+// #include "ConatctSystem/Resources.h"
 #include "hv/HttpService.h"
 
 class APIRouter{
-    public:
-
+ public:
     static int pre(HttpRequest* req, HttpResponse* resp) {
         resp->content_type = APPLICATION_JSON;
         req->ParseBody();
@@ -20,34 +19,34 @@ class APIRouter{
     }
 
     static int post(HttpRequest* req, HttpResponse* resp) {
-        //BYPASSUNUSED(req);
+        // BYPASSUNUSED(req);
         return 0;
     }
 
-    static void register_router(HttpService& router) {
-        router.preprocessor = pre;
-        router.postprocessor = post;
+    static void register_router(HttpService* router) {
+        router->preprocessor = pre;
+        router->postprocessor = post;
         
-        router.GET("", [](HttpRequest* req, HttpResponse* resp) {
+        router->GET("", [](HttpRequest* req, HttpResponse* resp) {
             return 404;
         });
 
-        router.POST("", [](HttpRequest* req, HttpResponse* resp) {
+        router->POST("", [](HttpRequest* req, HttpResponse* resp) {
             return 404;
         });
 
-        router.PUT("", [](HttpRequest* req, HttpResponse* resp) {
+        router->PUT("", [](HttpRequest* req, HttpResponse* resp) {
             return 404;
         });
 
-        router.Delete("", [](HttpRequest* req, HttpResponse* resp) {
+        router->Delete("", [](HttpRequest* req, HttpResponse* resp) {
             return 404;
         });
 
-        router.PATCH("", [](HttpRequest* req, HttpResponse* resp) {
+        router->PATCH("", [](HttpRequest* req, HttpResponse* resp) {
             return 404;
         });
     }
 };
 
-#endif
+#endif // BACKEND_APIROUTER_H_
