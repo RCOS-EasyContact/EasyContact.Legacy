2258e922cfcfd1ab14f70b0bc43e093865b77e51
Zhenyuan Gong <79224316+reC4P7CH4@users.noreply.github.com>
2021-10-03 21:07:40 -0400

    Tri

--- Backend/ContactSystem/Triple.h
+++ Backend/ContactSystem/Triple.h
@@ -6,15 +6,23 @@
  */
 #ifndef BACKEND_CONTACTSYSTEM_TRIPLE_H_
 #define BACKEND_CONTACTSYSTEM_TRIPLE_H_
+/**
+ * EasyContact Custom Namespace
+ * BCS : Backend Contact System
+ */
 namespace BCS {
+/**
+ * Template Class
+ * <Triple> Repersents
+ * A Storage For Three Elements
+ */
 template <class A, class B, class C> class Triple {
+ public:
   A first;
   B second;
   C third;
-
-public:
   Triple<A, B, C>(const A &ArgA, const B &ArgB, const C &ArgC)
       : first(ArgA), second(ArgB), third(ArgC) {}
 };
-} // namespace BCS
-#endif // BACKEND_CONTACTSYSTEM_TRIPLE_H_
+}  // namespace BCS
+#endif  // BACKEND_CONTACTSYSTEM_TRIPLE_H_
