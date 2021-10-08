65c29980a959d99ed4d412cee0a7f86b00413481
Daxin-H <huangd5@rpi.edu>
2021-10-08 17:11:57 -0400

    fix name

---
+++ Frontend/src/App.js
@@ -0,0 +1,16 @@
+
+function App() {
+  return (
+    <div>
+      <h1>My Login_temp_page</h1>
+      <div className='card'>
+        <h2>Login</h2>
+        <div className='actions'>
+          <button className='btn'>Delete</button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
