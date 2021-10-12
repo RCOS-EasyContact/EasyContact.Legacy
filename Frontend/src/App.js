bcce7d13091bff0518323900fbb0140af1b33822
Daxin-H <huangd5@rpi.edu>
2021-10-12 16:25:44 -0400

    Fix react

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,14 +1,10 @@
+import Todo from './components/Todo';
 
 function App() {
   return (
     <div>
-      <h1>My Login_temp_page</h1>
-      <div className='card'>
-        <h2>Login</h2>
-        <div className='actions'>
-          <button className='btn'>Delete</button>
-        </div>
-      </div>
+      <h1>Hello</h1>
+      <Todo text='Login' />
     </div>
   );
 }
