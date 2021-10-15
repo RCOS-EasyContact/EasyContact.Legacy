f951e7aea1f8babf39589be0f1c1792c0ff011ba
Zhenyuan Gong <79224316+reC4P7CH4@users.noreply.github.com>
2021-10-15 00:19:30 -0400

    Update MainExecutable.cpp

--- Backend/MainExecutable.cpp
+++ Backend/MainExecutable.cpp
@@ -6,11 +6,19 @@
  */
 #ifndef BACKEND_MAINEXECUTABLE_CPP_
 #define BACKEND_MAINEXECUTABLE_CPP_
+// C Standard Library
+#include <signal.h>
+#include <stdlib.h>
+#include <sys/wait.h>
+#include <unistd.h>
+// C++ Standard Library
+#include <iostream>
 // EasyContact Header Files
-#include "SignalHandler.h"
+#include "ContactSystem/Key.hpp"
+#include "ContactSystem/Books.hpp"
 // Global Representation
-// BCS::ContactBook *ContactBookSystem;
-pthread_mutex_t CBS_Mutex;
+BCS::Books ContactBook;
+pthread_mutex_t ContactBook_Mutex;
 int main() {
   // Reg Sigactions
   REG_SIGACTION();
