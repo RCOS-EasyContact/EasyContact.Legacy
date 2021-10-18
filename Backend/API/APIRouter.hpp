fff5964cb894d77d36796a017edd22a479f88a13
jacky <jackyxu866@gmail.com>
2021-10-18 13:31:36 -0400

    Implement Login + Tags

--- Backend/API/APIRouter.hpp
+++ Backend/API/APIRouter.hpp
@@ -7,6 +7,8 @@
 #ifndef BACKEND_API_APIROUTER_HPP_
 #define BACKEND_API_APIROUTER_HPP_
 #include "hv/HttpService.h"
+#include "../ContactSystem/Books.hpp"
+
 class APIRouter{
  public:
     static int pre(HttpRequest* req, HttpResponse* resp) {
@@ -23,6 +25,47 @@ class APIRouter{
         router->preprocessor = pre;
         router->postprocessor = post;
 
+        // login
+        router->POST("/login/:userName/:password", 
+            [](HttpRequest* req, HttpResponse* resp) {
+            std::string userName = req->GetParam("userName");
+            std::string password = req->GetParam("password");
+            if(userName == "" || password == "") { return 400; }
+            if(true){ return 200; }
+            return 505;
+        });
+
+        // tags
+        router->GET("/tags/:tagName", [](HttpRequest* req, HttpResponse* resp) {
+            std::string tagName = req->GetParam("tagName");
+            if(tagName == ""){ return 400; }
+            std::unordered_set<std::string> contacts = 
+                getTagContains(tagName);
+            if(contacts.empty()){ return 404; }
+            for(std::string& name : contacts){
+                resp->json.push_back({"name", name});
+                return 200;
+            }
+            return 400;
+        });
+
+        router->POST("/tags/:tagName", 
+            [](HttpRequest* req, HttpResponse* resp) {
+            std::string tagName = req->GetParam("tagName");
+            if(tagName == ""){ return 400; }
+            if(newTag(tagName)){ return 200; }
+            return 406;
+        });        
+
+        router->Delete("/tags/:tagName", [](HttpRequest* req, HttpResponse* resp) {
+            std::string tagName = req->GetParam("tagName");
+            if(tagName == ""){ return 400; }
+            if(removeTag(tagName)){ return 200; }
+            return 404;
+        });
+
+
+        // templates [delete after finish]
         router->GET("", [](HttpRequest* req, HttpResponse* resp) {
             return 404;
         });
