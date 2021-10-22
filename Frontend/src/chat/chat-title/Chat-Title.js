9389b7a4cb720df607b38a03f9949df31622875f
Daxin-H <huangd5@rpi.edu>
2021-10-22 16:46:43 -0400

    fix delet icon

--- Frontend/src/chat/chat-title/Chat-Title.js
+++ Frontend/src/chat/chat-title/Chat-Title.js
@@ -6,7 +6,7 @@ function ChatTitle() {
     return (
         <div id="chat-title">
             <span>Daryl Duckmanton</span>
-            <img src={require("../../images/icons/trash-logo.svg")} alt="Delete Conversation" />
+            <img src={require("../../images/icons/trash-logo.svg").default} alt="Delete Conversation" />
         </div>
     );
 }
