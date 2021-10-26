9ce7a4a4f5617a40a1ab48d88f8c4c1d812198a0
Zhenyuan Gong <79224316+reC4P7CH4@users.noreply.github.com>
2021-10-26 16:58:48 -0400

    new

---
+++ Backend/Contacts/ContactReadWrite.hpp
@@ -0,0 +1,18 @@
+/**
+ * RCOS-EasyContact
+ * EasyContact/Backend/Contacts
+ * ContactReadWrite.hpp
+ * Copyright [2021] <RCOS-EasyContact>
+ */
+#ifndef BACKEND_DISKRW_CONTACTREADWRITE_HPP_
+#define BACKEND_DISKRW_CONTACTREADWRITE_HPP_
+// C++ Standard Library
+#include <iostream>
+#include <sstream>
+#include <string>
+// Standard Template Library
+#include <vector>
+namespace BCS{
+  
+}
+#endif // BACKEND_DISKRW_CONTACTREADWRITE_HPP_
