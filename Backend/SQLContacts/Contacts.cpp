482cbb99525d2144eb8cb177a6207ef50087b966
Zhenyuan Gong <79224316+reC4P7CH4@users.noreply.github.com>
2021-10-30 01:48:38 -0400

    test5

--- Backend/SQLContacts/Contacts.cpp
+++ Backend/SQLContacts/Contacts.cpp
@@ -40,8 +40,8 @@ void BCS::Contacts::newTag(const std::string& TagName) {
     //   Query.bind(1, TagName);
     //   Query.exec();
     // }
-    SQLite::Statement Query(DB3, "CREATE TABLE :TableName (RCSID TEXT NOT NULL PRIMARY KEY)");
-    Query.bind(":TableName", "tag_"+TagName);
+    const std::string NewTableName="tag_"+TagName;
+    SQLite::Statement Query(DB3, "CREATE TABLE "+NewTableName+" (RCSID TEXT NOT NULL PRIMARY KEY)");
     Query.exec();
   } catch (std::exception& Err) {
     std::cerr << "Run-Time Exception <SQLite> := " << Err.what() << std::endl;
