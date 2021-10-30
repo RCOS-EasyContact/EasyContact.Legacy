6367005a18c97eadb08fc0c8bee22ad295b353cb
Zhenyuan Gong <79224316+reC4P7CH4@users.noreply.github.com>
2021-10-29 20:31:51 -0400

    SQLContacts- Reconstruction Rev.2 Part.1 (#33)
    * re
    
    * new
    
    * aa
    
    * giti
    
    * cmake
    
    * cmake
    
    * big changes
    
    * NEW EMPTY DATABASE WITH EMAILADRES TABLE
    
    * newContact DONE
    
    * minor changes
    
    * Update CodeQL(CPP).yml
    
    * Update Contacts.cpp
    
    * Update Contacts.cpp
    
    * [BOT] Clang-Format --Style=Google
    
    * Update CodeQL(CPP).yml
    
    * Update .gitignore
    
    * +
    
    * [BOT] Clang-Format --Style=Google
    
    * Update Contacts.cpp
    
    * Update Contacts.hpp
    
    * [BOT] Clang-Format --Style=Google
    
    * [BOT] Clang-Format --Style=Google
    
    * Create BCSHelpers.hpp
    
    * newTag, getAllTags DONE
    
    * [BOT] Clang-Format --Style=Google
    
    * add all the functions back (not implemented)
    
    * [BOT] Clang-Format --Style=Google
    
    * Delete BCSHelpers.hpp
    
    * Delete UserData/gongz2 directory
    
    * Create README.md
    
    * Update README.md
    
    * remove Comments
    
    * Create README.md
    
    * Update Contacts.hpp
    
    * public attrib -> private
    
    * [BOT] Clang-Format --Style=Google
    
    * [[nodiscard]]
    
    * [BOT] Clang-Format --Style=Google
    
    * Create README.md
    
    * fix: cmake include source cpps
    
    * Bug Fix
    
    * feat: include mailio into CMake
    
    * fix: action install dependencies
    
    * fix: libboost dependency
    
    * fix: SQLiteCpp hpp paths
    
    * Update Contacts.hpp
    
    * Update Contacts.hpp
    
    * Update CodeQL(CPP).yml
    
    Co-authored-by: EasyContact-RCOS <92967192+EasyContact-RCOS@users.noreply.github.com>
    Co-authored-by: Viyerelu23333 <11363469+Viyerelu23333@users.noreply.github.com>

---
+++ Backend/Executable/MultiClientMain.cpp
@@ -0,0 +1,27 @@
+/**
+ * RCOS-EasyContact
+ * EasyContact/Backend/Executable
+ * MultiClientMain.cpp
+ * Copyright [2021] <RCOS-EasyContact>
+ */
+#ifndef BACKEND_EXECUTABLE_MULTICLIENTMAIN_CPP_
+#define BACKEND_EXECUTABLE_MULTICLIENTMAIN_CPP_
+// C Standard Library
+#include <signal.h>
+#include <stdlib.h>
+#include <sys/wait.h>
+#include <unistd.h>
+// C++ Standard Library
+#include <iostream>
+#include <mutex>
+#include <thread>
+// Standard Template Library
+#include <unordered_map>
+// EasyContact Header Files
+// #include "../API/APIRouter.hpp"
+#include "../SQLContacts/Contacts.hpp"
+#include "GlobalMutex.hpp"
+// Global Representation
+std::unordered_map<std::string, BCS::Contacts> ActiveUsers;
+int main() {}
+#endif  // BACKEND_EXECUTABLE_MULTICLIENTMAIN_CPP_
---
+++ Backend/SQLContacts/Contacts.hpp
@@ -0,0 +1,47 @@
+/**
+ * RCOS-EasyContact
+ * EasyContact/Backend/SQLContacts
+ * Contacts.hpp
+ * Copyright [2021] <RCOS-EasyContact>
+ */
+#ifndef BACKEND_SQLCONTACTS_CONTACTS_HPP_
+#define BACKEND_SQLCONTACTS_CONTACTS_HPP_
+// SQL Lite Library
+#include <SQLiteCpp/SQLiteCpp.h>
+#include <SQLiteCpp/VariadicBind.h>
+// C++ Standard Library
+#include <exception>
+#include <filesystem>
+#include <iostream>
+#include <string>
+// Standard Template Library
+#include <vector>
+/**
+ * EasyContact Custom Namespace
+ * BCS : Backend Contact System
+ */
+namespace BCS {
+void CreateDirectory(const std::string &DirName);
+class Contacts {
+  const std::string RCSID;
+
+ public:
+  explicit Contacts(const std::string &newRCSID) : RCSID(newRCSID) {
+    CreateDirectory(RCSID);
+  }
+  bool newContact(const std::string &Name, const std::string &Email);
+  void newTag(const std::string &TagName);
+  bool removeContact(const std::string &ExistContact);
+  bool removeTag(const std::string &TagName);
+  [[nodiscard]] std::vector<std::string> getAllContacts() const;
+  [[nodiscard]] std::vector<std::string> getAllTags() const;
+  [[nodiscard]] std::vector<std::string> getTagContains(
+      const std::string &TagName) const;
+  [[nodiscard]] std::vector<std::string> getNameInTags(
+      const std::string &ContactName) const;
+  void assignTagTo(const std::string &TagName, const std::string &ContactName);
+  void removeTagFor(const std::string &TagName, const std::string &ContactName);
+  void clearTagFor(const std::string &ContactName);
+};
+}  // namespace BCS
+#endif  // BACKEND_SQLCONTACTS_CONTACTS_HPP_
