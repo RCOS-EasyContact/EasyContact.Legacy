69113d20afb553172db8e4591221fb6743eda496
Zhenyuan Gong <79224316+reC4P7CH4@users.noreply.github.com>
2021-11-06 21:31:23 +0000

    fix: buffer

--- Backend/Executable/SysLogs.hpp
+++ Backend/Executable/SysLogs.hpp
@@ -14,9 +14,11 @@ namespace SYSLOG {
 static void PrintException(const std::exception &Err) {
   time_t _TT;
   struct tm *_TI;
+  char _B1[26];
+  char _B2[26];
   time(&_TT);
-  _TI = localtime_r(&_TT, 0);
-  std::cerr << asctime_r(_TI, 0) << "Run-Time Exception: " << Err.what()
+  _TI = localtime_r(&_TT, &_B1);
+  std::cerr << asctime_r(_TI, &_B2) << "Run-Time Exception: " << Err.what()
             << std::endl;
 }
 }  // namespace SYSLOG
