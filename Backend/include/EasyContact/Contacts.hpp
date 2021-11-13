09d9c96f95a40aa9d1b85899e999a1f2f3af88a6
Zhenyuan Gong <79224316+reC4P7CH4@users.noreply.github.com>
2021-11-13 02:28:53 +0000

    fix: gitignore

---
+++ Backend/include/EasyContact/Contacts.hpp
@@ -0,0 +1,43 @@
+/**
+ * RCOS-EasyContact
+ * EasyContact/Backend/SQLContacts
+ * Contacts.hpp
+ * Copyright [2021] <RCOS-EasyContact>
+ */
+#ifndef BACKEND_SQLCONTACTS_CONTACTS_HPP_
+#define BACKEND_SQLCONTACTS_CONTACTS_HPP_
+// SQL Lite Library
+#include <SQLiteCpp/SQLiteCpp.h>
+#include <SQLiteCpp/VariadicBind.h>
+// C++ Standard Library
+#include <filesystem>
+#include <string>
+// Standard Template Library
+#include <vector>
+// EasyContact Header Files
+#include "EasyContact/SysLogs.hpp"
+/**
+ * EasyContact Custom Namespace
+ * BCS : Backend Contact System
+ */
+namespace BCS {
+void CreateDirectory(const std::string &DirName);
+class Contacts {
+ private:
+  const std::string RCSID;
+
+ public:
+  explicit Contacts(const std::string &newRCSID);
+  bool newContact(const std::string &Name, const std::string &Email);
+  bool newTag(const std::string &TagName);
+  bool removeContact(const std::string &Name);
+  bool removeTag(const std::string &TagName);
+  std::string getEmailAddress(const std::string &Name) const;
+  std::vector<std::string> getAllNames() const;
+  std::vector<std::string> getAllTags() const;
+  std::vector<std::string> getTagContains(const std::string &TagName) const;
+  bool assignTagTo(const std::string &TagName, const std::string &ContactName);
+  bool removeTagFor(const std::string &TagName, const std::string &ContactName);
+};
+}  // namespace BCS
+#endif  // BACKEND_SQLCONTACTS_CONTACTS_HPP_
