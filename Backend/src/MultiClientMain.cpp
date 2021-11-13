086d78c52fd42a1715ff6cf81f0eaee46bc25038
Zhenyuan Gong <79224316+reC4P7CH4@users.noreply.github.com>
2021-11-13 16:49:42 -0500

    Update MultiClientMain.cpp

--- Backend/src/MultiClientMain.cpp
+++ Backend/src/MultiClientMain.cpp
@@ -6,10 +6,6 @@
  */
 #ifndef BACKEND_SRC_MULTICLIENTMAIN_CPP_
 #define BACKEND_SRC_MULTICLIENTMAIN_CPP_
-// Resolve Conflict with LibBoost
-#ifdef defer
-#undef defer
-#endif
 // C Standard Library
 #include <signal.h>
 #include <stdlib.h>
