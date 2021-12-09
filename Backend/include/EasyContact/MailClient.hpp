5fc3aa35fd38636d812f9d16ef4bb35fc18fcee4
Zhenyuan Gong <79224316+reC4P7CH4@users.noreply.github.com>
2021-12-08 20:29:57 -0500

    fix: remove existing implementation of MailClient

--- Backend/include/EasyContact/MailClient.hpp
+++ Backend/include/EasyContact/MailClient.hpp
@@ -6,46 +6,25 @@
  */
 #ifndef BACKEND_INCLUDE_EASYCONTACT_MAILCLIENT_HPP_
 #define BACKEND_INCLUDE_EASYCONTACT_MAILCLIENT_HPP_
-// Resolve Conflict with LibBoost
-#ifdef defer
-#undef defer
-#endif
 // C++ Standard Library
-#include <algorithm>
-#include <fstream>
-#include <iostream>
 #include <string>
-// Standard Template Library
-#include <list>
-#include <utility>
-// MailIO Header Files
-#include <mailio/imap.hpp>
-#include <mailio/message.hpp>
-#include <mailio/smtp.hpp>
-// EasyContact Header Files
-#include <EasyContact/SysLogs.hpp>
 namespace BMC {
-bool AuthenticateLogin(const std::string& RCSID, const std::string& Password);
+bool AuthenticateLogin(const std::string& RCSID, const std::string& Password) noexcept;
 class MailClient {
  private:
-  const std::string RCSID;
-  const std::string Password;
-  const std::string EmailAddress;
-  mutable std::string Nickname;
-
- protected:
-  [[nodiscard]] size_t InboxEmails() const;
-  [[nodiscard]] bool RecvEmail(const size_t& ID, mailio::message* M) const;
-  [[nodiscard]] bool RemoveEmail(const size_t& ID);
+  const std::string __RCSID;
+  const std::string __Password;
+  const std::string __EmailAddress;
+  std::string __Nickname;
 
  public:
-  explicit MailClient(const std::string& _RCSID, const std::string& _Password);
-  void ChangeNickname(const std::string& _Nickname) noexcept;
-  bool Fetch(const size_t& NumEmails) const;
+  explicit MailClient(const std::string& RCSID, const std::string& Password);
+  void ChangeNickname(const std::string& Nickname) noexcept;
+  bool FetchEmails(const size_t& NumEmails) const noexcept;
   bool SendMessage(const std::string& Recipient_Name,
                    const std::string& Recipient_Email,
                    const std::string& Subject,
-                   const std::string& MessageBody) const;
+                   const std::string& MessageBody) const noexcept;
 };
 }  // namespace BMC
 #endif  // BACKEND_INCLUDE_EASYCONTACT_MAILCLIENT_HPP_
