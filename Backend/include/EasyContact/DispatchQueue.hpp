5687bcfd2d4b51e9f3ebe4c9d6b87200920345f9
Zhenyuan Gong <79224316+reC4P7CH4@users.noreply.github.com>
2021-12-08 20:08:58 -0500

    Document - Class DispatchQueue (#48)
    * feat: document DQ.hpp
    
    * fix: typedef -> using =
    
    * fix: move include
    
    * fix: separate unused
    
    * fix: private -> protected
    
    * fix: construct
    
    * fix: remove unsed include

--- Backend/include/EasyContact/DispatchQueue.hpp
+++ Backend/include/EasyContact/DispatchQueue.hpp
@@ -11,36 +11,77 @@
 // C++ Standard Library
 #include <condition_variable>
 #include <functional>
-#include <iostream>
 #include <mutex>
-#include <string>
 #include <thread>
 // Standard Template Library
 #include <queue>
 #include <utility>
 #include <vector>
-// EasyContact Header Files
-#include <EasyContact/SingleUser.hpp>
 class DispatchQueue {
-  typedef std::function<void(void)> Functor;
+  using Functor = std::function<void(void)>;
 
  private:
+  bool inServices;
   size_t JobID;
   std::mutex Lock;
   std::vector<std::thread> Threads;
   std::queue<Functor> Queue;
   std::condition_variable CV;
-  bool inServices = true;
+
+ protected:
   void Dispatch_Hander(void);
 
  public:
+  /**
+   * Default Class Constructor
+   * @param NumThreads : Number of Threads to Create
+   */
   explicit DispatchQueue(const size_t& NumThreads);
+  /**
+   * Default Class Destructor
+   */
   ~DispatchQueue();
+  /**
+   * Add Task to Dispatch Queue
+   * @param Operation : Task Expressed in Lambda Expression
+   * @return std::pair<size_t, size_t> : Your Task ID, Number of Tasks Before
+   * Your Task
+   */
   std::pair<size_t, size_t> Dispatch(const Functor& Operation);
+  /**
+   * Add Task to Dispatch Queue
+   * @param Operation : Task Expressed in Lambda Expression
+   * @return std::pair<size_t, size_t> : Your Task ID, Number of Tasks Before
+   * Your Task
+   */
   std::pair<size_t, size_t> Dispatch(Functor&& Operation);
+
+ public:
+  /**
+   * This Funtion Call Is Not Allowed &
+   * Will Delete Left Hand Side Instance
+   * @param RHS : Another Instance of Current Class
+   */
   DispatchQueue(const DispatchQueue& RHS) = delete;
+  /**
+   * This Funtion Call Is Not Allowed &
+   * Will Delete Left Hand Side Instance
+   * @param RHS : Another Instance of Current Class
+   * @return DispatchQueue : Reference to SELF
+   */
   DispatchQueue& operator=(const DispatchQueue& RHS) = delete;
+  /**
+   * This Funtion Call Is Not Allowed &
+   * Will Delete Left Hand Side Instance
+   * @param RHS : Another Instance of Current Class
+   */
   DispatchQueue(DispatchQueue&& RHS) = delete;
+  /**
+   * This Funtion Call Is Not Allowed &
+   * Will Delete Left Hand Side Instance
+   * @param RHS : Another Instance of Current Class
+   * @return DispatchQueue : Reference to SELF
+   */
   DispatchQueue& operator=(DispatchQueue&& RHS) = delete;
 };
 #endif  // BACKEND_INCLUDE_EASYCONTACT_DISPATCHQUEUE_HPP_
